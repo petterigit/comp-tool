04bed9d26a39dc2aafbc5d210dbb359500a20fc8
vilperi <p-makela@outlook.com>
2025-07-11 11:53:56 +0300

    Switch canvas to @napi-rs/canvas

--- src/routes/api/comp-image/+server.ts
+++ src/routes/api/comp-image/+server.ts
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage } from '@napi-rs/canvas';
 import type { RequestHandler } from '@sveltejs/kit';
 import { classColors, compToRoles, decodeComp } from '$lib/util/data';
 import { sortByClassSpec } from '$lib/util/sort';
